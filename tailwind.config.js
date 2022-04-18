0e57cad7652f52980548a8f5894a9e0297aae2fb
Dominik Obloza <main@dominikobloza.pl>
2022-04-18 23:17:01 +0200

    Init tailwind, add custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                justDark: {
+                    black: '#111111',
+                    light: '#242424'
+                },
+                justWhite: {
+                    light: '#EAEAEA',
+                    dark: '#525252'
+                },
+                extra: {
+                    color: '#FACF87'
+                }
+            }
+        },
+    },
+    plugins: [],
+}
